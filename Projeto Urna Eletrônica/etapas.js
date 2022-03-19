814f21bd70d92f63e0ae02a93082d2015aca2d01
Leandro Matheus <87612793+Leandro-Matheus-Gomes@users.noreply.github.com>
2022-03-18 21:05:45 -0300

    Projeto concluído
    Um pequeno projeto que permite interação com um usuário, voltado para front-end, e focado na linguagem JavaScript.

---
+++ Projeto Urna Eletrônica/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '55555',
+                nome: 'Monkey D. Luffy',
+                partido: 'CDP',
+                fotos: [
+                    {url:'Luffy.jpeg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '33333',
+                nome: 'Manjiro Sano',
+                partido: 'TOMAN',
+                fotos: [
+                    {url:'Mikey.jpeg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '88',
+                nome: 'Roronoa Zoro',
+                partido: 'CDP',
+                vice: 'Vinsmoke Sanji',
+                fotos: [
+                    {url:'Zoro.jpeg', legenda: 'Prefeito'},
+                    {url:'Sanji.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '77',
+                nome: 'Ken Ryuguji',
+                partido: 'TOMAN',
+                vice: 'Takashi Mitsuya',
+                fotos: [
+                    {url:'Draken.jpeg', legenda: 'Prefeito'},
+                    {url:'Mitsuya.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
